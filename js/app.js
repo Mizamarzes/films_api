858c2a39cdc32f4913e097becbbfef05b3580a8d
mizamarzes <mizamarzes@gmail.com>
2024-02-28 17:21:27 -0500

    fix that

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ btnAnterior.addEventListener('click', () => {
 
 const cargarPeliculas = async() => {
 	try {
-		const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=192e0b9821564f26f52949758ea3c473&language=es-MX&page=${pagina}`);	
+		const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=192e0b9821564f26f52949758ea3c473&language=es-MX&page=${page}`);	
 		console.log(respuesta);
 
 		if(respuesta.status === 200){
